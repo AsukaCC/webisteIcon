f4a0095827dd69299592f0665f05891a17a95d7a
JIANGCHENG <plutavian@gmail.com>
2025-01-10 15:02:40 +0800

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [{
+    name: "website-icon-service",
+    script: "./dist/app.js",
+    instances: 2,
+    exec_mode: "cluster",
+    env: {
+      NODE_ENV: "development",
+      PORT: 3000
+    },
+    env_production: {
+      NODE_ENV: "production",
+      PORT: 8080
+    },
+    watch: false,
+    max_memory_restart: '1G',
+    log_date_format: "YYYY-MM-DD HH:mm:ss",
+    error_file: "logs/err.log",
+    out_file: "logs/out.log",
+    merge_logs: true
+  }]
+};
