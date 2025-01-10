2a02de5ae5f3634ab8bd1a8fd7d81fee39c0153b
JIANGCHENG <plutavian@gmail.com>
2025-01-10 15:05:02 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,20 @@
 module.exports = {
-  apps: [{
-    name: "website-icon-service",
-    script: "./dist/app.js",
-    instances: 2,
-    exec_mode: "cluster",
-    env: {
-      NODE_ENV: "development",
-      PORT: 3000
+  apps: [
+    {
+      name: 'website-icon-service',
+      script: './dist/app.js',
+      instances: 1, // 设置为1表示单实例运行
+      exec_mode: 'fork', // 使用 fork 模式（单实例模式）
+      env: {
+        NODE_ENV: 'production', // 环境变量
+        PORT: 8083, // 服务端口
+      },
+      watch: false, // 禁用文件监听
+      max_memory_restart: '1G', // 内存超限自动重启
+      log_date_format: 'YYYY-MM-DD HH:mm:ss', // 日志时间格式
+      error_file: 'logs/err.log', // 错误日志文件
+      out_file: 'logs/out.log', // 输出日志文件
+      merge_logs: true, // 合并日志文件
     },
-    env_production: {
-      NODE_ENV: "production",
-      PORT: 8083
-    },
-    watch: false,
-    max_memory_restart: '1G',
-    log_date_format: "YYYY-MM-DD HH:mm:ss",
-    error_file: "logs/err.log",
-    out_file: "logs/out.log",
-    merge_logs: true
-  }]
+  ],
 };
